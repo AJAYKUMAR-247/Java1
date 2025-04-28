99220107ad1f5c6ff94231a70535bff2bf9f54c5
Ajaykumar Vijayakumar <ajay.k@ycs.in>
2025-04-28 21:36:14 +0530

    Files Added

--- src/Array/Array1.java
+++ src/Array/Array1.java
@@ -8,7 +8,6 @@ public class Array1 {
 
         Scanner sc = new Scanner(System.in);
 
-
         int[] arr1 = new int[10];
 
         for (int i = 0; i < arr1.length; i++) {
---
+++ src/ArrayList/Arraylist.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Arraylist {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer > list = new ArrayList<>();
+
+        list.add(23);
+
+        System.out.println(list);
+
+    }
+}
